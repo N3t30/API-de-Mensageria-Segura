5f04a2109f690feb31f6283558bc52e76feeafec
Neto Peixoto <joseneto55peixoto@gmail.com>
2026-01-23 21:36:29 -0300

    Bloquando acessos indevidos e registrando tentativas invalidas

--- Chat/app/accounts/views.py
+++ Chat/app/accounts/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import Message
+from .models import AuditLog, Message
 from .serializers import MessageSerializer, RegisterSerializer
 
 
@@ -30,13 +30,47 @@ class LogoutView(APIView):
 usa hash e bloqueia usuarios já autenticaddos de se registrar"""
 
 
+''' Só quem participa vê com controle de acesso e Admin vê tudo'''
+
+
 class MessageListView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        messages = Message.objects.filter(
-            Q(sender=request.user) | Q(recipient=request.user)
-        )
+        if request.user.is_staff:
+            messages = Message.objects.all()
+        else:
+            messages = Message.objects.filter(
+                Q(sender=request.user) | Q(recipient=request.user)
+            )
 
         serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+
+class MessageDetailView9APIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, message_id):
+        try:
+            message = Message.objects.get(id=message_id)
+        except Message.DoesNotExist:
+            return Response(status=404)
+
+        # regra de négocio
+        is_participant = (
+            message.sender == request.user or
+            message.recipient == request.user
+        )
+
+        if not is_participant and not request.user.is_staff:
+            AuditLog.objects.create(
+                user=request.user,
+                action="ANAUTHORIZED-ACCESS",
+                target=f"message:{message.id}",
+                ip_address=request.META.get("REMOTE_ADDR")
+            )
+            return Response(status=403)
+
+        serializer = MessageSerializer(message)
         return Response(serializer.data)
