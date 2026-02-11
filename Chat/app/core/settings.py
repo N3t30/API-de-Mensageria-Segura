7cdcfb2135d3110586b8cf0c5c61fb863d2586bc
Neto Peixoto <joseneto55peixoto@gmail.com>
2026-02-10 22:32:33 -0300

    Preparando ambinete para implementação Websocket

--- Chat/app/core/settings.py
+++ Chat/app/core/settings.py
@@ -98,7 +98,16 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.core.wsgi.application"
+ASGI_APPLICATION = "app.core.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
